0580ff3a4a2e6b2d7864fbd2abd6c82dcafb52ee
Surjune <rajeswaris329@gmail.com>
2026-03-10 23:53:29 +0530

    Fix Vite base path for Vercel routing

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -11,6 +11,7 @@ export default defineConfig(({ command, mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
   
   return {
+     base: "/", 
     plugins: [react()],
     resolve: {
       alias: {
